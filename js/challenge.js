b969663beabc5f44b3681c283b917119e0319bc4
Qays Ayad <work.worker55@gmail.com>
2020-05-23 20:40:35 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   // console.log("%c DOM is loaded", "color :purple")
-  // 
+  //
   // const pageCounter = document.querySelector('#counter')
   // const pauseBtn = document.querySelector('#pause')
   // const incrementBtn = document.querySelector('#plus')
@@ -10,14 +10,14 @@ document.addEventListener('DOMContentLoaded', () => {
   // const commentList = document.querySelector('#commentList')
   // const commentInput = document.querySelector('#comment-input')
   // let numbersList = document.querySelector('#likesList')
-  // 
-  // 
+  //
+  //
   // let iter = 0
   // let pause = false
   // let currentCount = 0
   // let selectedNumber = 0
   // let likes = 0
-  // 
+  //
   // function counter() {
   //   if (pause) {
   //     return
@@ -26,12 +26,12 @@ document.addEventListener('DOMContentLoaded', () => {
   //     setTimeout(counter, 1000)
   //   }
   // }
-  // 
+  //
   // counter()
-  // 
+  //
   // pauseBtn.addEventListener('click', event => {
   //   console.log(event)
-  // 
+  //
   //   if (pause == false) {
   //     pause = true
   //     incrementBtn.disabled = true
@@ -47,27 +47,27 @@ document.addEventListener('DOMContentLoaded', () => {
   //     counter()
   //   }
   // })
-  // 
+  //
   // incrementBtn.addEventListener('click', event => {
   //   console.log(event)
-  // 
+  //
   //   iter = currentCount
   //   iter += 1
   //   pageCounter.innerHTML = iter
   // })
-  // 
+  //
   // decrementBtn.addEventListener('click', event => {
   //   console.log(event)
-  // 
+  //
   //   iter = currentCount
   //   iter -= 1
   //   pageCounter.innerHTML = iter
   // })
-  // 
+  //
   // heartBtn.addEventListener('click', event => {
   //   console.log(event)
   //   let number = currentCount
-  // 
+  //
   //   if (selectedNumber == number) {
   //     likes += 1
   //     numbersList.innerHTML += `<li> Number ${number} has ${likes} likes </li>`
@@ -78,11 +78,11 @@ document.addEventListener('DOMContentLoaded', () => {
   //       numbersList.innerHTML += `<li> Number ${number} has ${likes} likes </li>`
   //   }
   // })
-  // 
+  //
   // commentForm.addEventListener('submit', event => {
   //   event.preventDefault()
   //   debugger
-  // 
+  //
   //   let li = commentInput.value
   //   commentList.innerHTML += `<li> ${li} </li>`
   // })
